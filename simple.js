c436c53943a6b0d9359f237d2c424f587c02a07f
joel thomas mathew <joelmathew857@gmail.com>
2024-03-20 10:04:24 +0530

    walking indis

--- simple.js
+++ simple.js
@@ -1,25 +1,11 @@
 const e = require("express");
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 
-const datas = [
-  { name: "joel", id: 1 },
-  { name: "mathew", id: 2 },
-  { name: "thomas", id: 3 },
-];
-
-app.get("/user/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  if (!isNaN(id)) {
-    const foundUser = datas.find((item) => item.id === id);
-    if (foundUser) {
-      res.send(`User found: ${foundUser.name}`);
-    } else {
-      res.send("User not found");
-    }
-  } else {
-    res.send("You entered a string");
-  }
+app.get("/User/:id", (req, res) => {
+  const ids = parseInt(req.params.id);
+  res.send(`the id is ${ids}`);
 });
 
 app.listen(3000, () => {
